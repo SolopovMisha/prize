4d010a7edc292843d03a9e2beab293c8c3a9d665
Bezdar <100799406+SolopovMisha@users.noreply.github.com>
2026-01-14 19:48:55 +0400

    uploaded to git

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector('.prize__button').addEventListener('click', () => {
+  const elements = document.querySelectorAll('.prize_number');
+  const allPrizes = document.querySelectorAll('.prize');
+  const resultArea = document.querySelector('.prize__result');
+  const prizeNumbers = [];
+  
+  
+  for (let index = 0; index < elements.length; index++) {
+    prizeNumbers.push(elements[index].textContent);
+  }
+  
+  for (let cardIndex = 0; cardIndex < allPrizes.length; cardIndex++) {
+    allPrizes[cardIndex].classList.remove('active');
+  }
+  
+  const randomIndex = Math.floor(Math.random() * prizeNumbers.length);
+  const randomNumber = prizeNumbers[randomIndex];
+  
+  resultArea.innerHTML = randomNumber;
+  
+  const winningPrize = Array.from(elements).find(el => el.textContent === randomNumber).closest('.prize');
+  winningPrize.classList.add('active');
+  
+  console.log(prizeNumbers);
+});
